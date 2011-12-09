16c5917ee0bc1ed74e875df46257d4424386f13d
Peter Donald <peter@realityforge.org>
2011-12-10 08:36:13 +1100

    Add some collectd services

--- roles/metrics.rb
+++ roles/metrics.rb
@@ -2,7 +2,14 @@ name "metrics"
 description "Node to measure metrics across system"
 run_list(
   "role[base]",
-  "recipe[collectd]",
-  "recipe[collectd_plugins]",
+  "recipe[collectd::server]",
+  "recipe[collectd::client]",
+  "recipe[collectd_plugins::syslog]]",
+  "recipe[collectd_plugins::cpu]",
+  "recipe[collectd_plugins::df]",
+  "recipe[collectd_plugins::disk]",
+  "recipe[collectd_plugins::interface]",
+  "recipe[collectd_plugins::memory]",
+  "recipe[collectd_plugins::swap]",
   "recipe[graphite]"
 )
