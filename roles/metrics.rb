d2a886b7510d432bcdbcc347ac3d8010640f7b8f
Peter Donald <peter@realityforge.org>
2011-12-10 10:08:07 +1100

    Temporaril disable collectd client

--- roles/metrics.rb
+++ roles/metrics.rb
@@ -3,7 +3,7 @@ description "Node to measure metrics across system"
 run_list(
   "role[base]",
   "recipe[collectd::server]",
-  "recipe[collectd::client]",
+  #"recipe[collectd::client]",
   "recipe[collectd_plugins::syslog]",
   "recipe[collectd_plugins::cpu]",
   "recipe[collectd_plugins::df]",
